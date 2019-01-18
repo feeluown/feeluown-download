e2389fe7ce204ea9d89666178de7cb223cadeafc
cosven <yinshaowen241@gmail.com>
2019-01-19 01:53:14 +0800

    add Downloader class

--- fuo_dl/__init__.py
+++ fuo_dl/__init__.py
@@ -1,3 +1,16 @@
+from concurrent.futures import ThreadPoolExecutor
+
+
+__alias__ = '音乐下载'
+__desc__ = '音乐下载插件'
+__version__ = '0.1'
+
+
+class Downloader:
+    def __init__(self, max_workers=None):
+        self.pool = ThreadPoolExecutor(max_workers=max_workers)
+
+
 def enable(app):
     pass
 
