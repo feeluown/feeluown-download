f41d9ccf1fcba234a723d8027f6ef137e309a838
cyliuu <lcy940903@gmail.com>
2021-12-22 23:04:57 +0800

    * remove fuocore  * fix pyqt error in -nw mode  * support fuo_dl config  * use async in prepare_filename and tag when network requesting happens  * split download and tagger

--- fuo_dl/__init__.py
+++ fuo_dl/__init__.py
@@ -6,7 +6,7 @@ fuo_dl 是 FeelUOwn 的一个音乐下载插件。
 fuo_dl 支持多首歌并行下载，也支持一首歌分多段并行下载（也就是常说的多线程下载）。
 另外，用户可以在 ~/.fuorc 中自定义下载路径::
 
-   config.DOWNLOAD_DIR = '~/Music'
+   config.fuo_dl.DOWNLOAD_DIR = '~/Music'
 """
 
 import logging
@@ -25,11 +25,15 @@ dm_mgr = None
 dm_ui = None
 
 
-def enable(app):
+def init_config(config):
+    config.deffield('DOWNLOAD_DIR', type_=str, default=None, desc='')
+
+
+def autoload(app):
     global dm_mgr, dm_ui
 
     # initialize download manager
-    dm_mgr = dm_mgr or DownloadManager(app)
+    dm_mgr = dm_mgr or DownloadManager(app, app.config.fuo_dl)
     dm_mgr.initialize()
 
     # initialize ui for download manager
@@ -38,6 +42,11 @@ def enable(app):
 
         dm_ui = dm_ui or DownloadUi(dm_mgr, app, app.ui)
         dm_ui.initialize()
+
+
+def enable(app):
+    app.initialized.connect(lambda *args: autoload(*args),
+                            weak=False, aioqueue=True)
 
 
 def disable(app):
