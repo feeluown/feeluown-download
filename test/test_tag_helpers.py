4f4ea50bb77494ea6785c3555970359eba9c8dde
cyliuu <lcy940903@gmail.com>
2022-01-19 22:14:30 +0800

    * complete test_function 	* fix a bug when cook_filepath

--- test/test_tag_helpers.py
+++ test/test_tag_helpers.py
@@ -1,4 +1,5 @@
 from fuo_dl.tag_helpers import *
+from feeluown.library.provider import DummySongModel, DummyAlbumModel, DummyArtistModel
 
 
 def test_beautify_tagobj():
@@ -9,11 +10,14 @@ def test_beautify_tagobj():
 
 def test_cook_tagobj():
     song = object()
-    song.title = 'title'
-    song.album_name = ' '
-    song.artists_name = 'artist_name'
-    song.artists = [object()]
-    song.artists.cover = 'cover_url'
+    artist = DummyArtistModel(identifier=1,
+                              name='artist_name',
+                              cover='cover_url')
+    song = DummySongModel(identifier=1,
+                          title='title',
+                          artists=[artist],
+                          duration=0)
+
     tag_obj, cover_url = cook_tagobj(song)
     assert tag_obj['title'] == 'title'
     assert tag_obj['artist'] == 'artist_name'
@@ -21,6 +25,11 @@ def test_cook_tagobj():
 
 
 def test_cook_filepath():
+
+    def get_filepath(tag_obj, ext, fmts):
+        storage_path, filename = cook_filepath(tag_obj, ext, fmts)
+        return os.path.join(storage_path, filename)
+
     tag_obj1 = {
         'title': 'ti',
         'artist': 'art',
@@ -29,10 +38,10 @@ def test_cook_filepath():
         'tracknumber': '1/1',
         'discnumber': '1/1',
     }
-    assert cook_filepath(tag_obj1, 'flac', ['{albumartist}/{album}/{track} {title}']) == 'alb_art/alb/02 ti.flac'
-    assert cook_filepath(tag_obj1, 'mp3', ['{albumartist}/{album}/{title}']) == 'alb_art/alb/ti.mp3'
-    assert cook_filepath(tag_obj1, 'mp3', ['{artist}/{title}']) == 'art/ti.mp3'
-    assert cook_filepath(tag_obj1, 'm4a', ['title - artist']) == 'til - art.m4a'
+    assert get_filepath(tag_obj1, 'flac', ['{albumartist}/{album}/{track} {title}']) == 'alb_art/alb/01 ti.flac'
+    assert get_filepath(tag_obj1, 'mp3', ['{albumartist}/{album}/{title}']) == 'alb_art/alb/ti.mp3'
+    assert get_filepath(tag_obj1, 'mp3', ['{artist}/{title}']) == 'art/ti.mp3'
+    assert get_filepath(tag_obj1, 'm4a', ['{title} - {artist}']) == 'ti - art.m4a'
 
     tag_obj2 = {
         'title': 'ti',
@@ -42,7 +51,7 @@ def test_cook_filepath():
         'tracknumber': '2/2',
         'discnumber': '1/2',
     }
-    assert cook_filepath(tag_obj2, 'flac', ['{albumartist}/{album}/{track}++{title}']) == 'alb_art/alb/1-02++ti.flac'
+    assert get_filepath(tag_obj2, 'flac', ['{albumartist}/{album}/{track}++{title}']) == 'alb_art/alb/1-02++ti.flac'
 
     tag_obj3 = {
         'title': 'ti',
@@ -50,5 +59,5 @@ def test_cook_filepath():
         'album': 'alb',
         'albumartist': 'alb_art',
     }
-    assert cook_filepath(tag_obj3, 'ape', ['{albumartist}/{album}/{track} {title}',
-                                           '{artist}/{title}']) == 'art/ti.ape'
+    assert get_filepath(tag_obj3, 'ape', ['{albumartist}/{album}/{track} {title}',
+                                          '{artist}/{title}']) == 'art/ti.ape'
